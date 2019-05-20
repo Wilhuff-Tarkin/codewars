a28189e8234cb19e80d72a9e9d58e839fe8d8723
Wilhuff-Tarkin <giantcaterpillar@gmail.com>
2019-05-20 22:48:56 +0200

    spinner solution

---
+++ src/StopGninnipSMySdroW.java
@@ -0,0 +1,44 @@
+/*
+Write a function that takes in a string of one or more words,
+and returns the same string, but with all five or more letter words reversed
+(Just like the name of this Kata). Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+ */
+
+
+public class StopGninnipSMySdroW {
+
+    public static String spinWords(String sentence) {
+        String [] array = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i].length() > 4) {
+                StringBuilder sb = new StringBuilder(array[i]);
+                result.append(sb.reverse().toString());
+                result.append(" ");
+            }
+            else {
+                result.append(array[i]);
+                result.append(" ");
+            }
+        }
+
+        //assign string builder to string and remove additional space at the end
+        sentence = result.toString().substring(0,result.length()-1);
+
+return sentence;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(spinWords("Hey fellow warriors"));
+    }
+}
